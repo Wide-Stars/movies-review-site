d8539f150acca3170388502872e5fa3324d123fa
hadiul-islam <hadiulislamofficial@gmail.com>
2022-09-19 16:57:23 +0600

    atuh context

---
+++ src/context/auth-context.js
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from 'react';
+
+
+const AuthContext = React.createContext({
+    isLoggedIn: false,
+    onLogout: () => {},
+    onLogin: (email, password) => {}
+});
+
+
+
+export const AuthContextProvider = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+
+
+
+  const loginHandler = (email, password) => {
+
+  };
+
+  const logoutHandler = () => {
+    
+  };
+
+    return (
+        <AuthContext.Provider value={{isLoggedIn: isLoggedIn, onLogout: logoutHandler, onLogin: loginHandler}}>
+            {props.children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthContext;
