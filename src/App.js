dc2dc40aa52a6e1d87f6dfbba4308f86e01217be
Maruf Rien <maruf14222@gmail.com>
2022-09-29 22:08:12 +0600

    feat: add login page

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import MovieList from "./components/dashboard/MovieList";
 import Admin from "./pages/Admin";
 import Details from "./pages/Details";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/movie/:id" element={<Details />} />
         <Route path="/dashboard" element={<Admin />} />
         <Route path="/dashboard/add_movie" element={<AddMove />} />
