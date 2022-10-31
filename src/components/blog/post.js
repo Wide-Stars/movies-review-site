5087c0666e56c540b9aa316c98052d32e04107b1
Maruf Rien <maruf14222@gmail.com>
2022-10-31 23:34:47 +0600

    feat: add icons

--- src/components/blog/post.js
+++ src/components/blog/post.js
@@ -1,15 +1,33 @@
 import React from 'react'
-import { FaClock } from "react-icons/fa";
+import { FaClock, FaEye } from "react-icons/fa";
 
 const post = () => {
 	return <div className="card">
 
 		<h5 className="card-title text text-dark">THE RETURN OF TANYA TUCKER: FEATURING BRANDI CARLILE</h5>
 
-		<FaClock />
-		<p className="card-text">
-			<small className="text-muted">10/10/10</small>
-		</p>
+		<div className='row'>
+
+		</div>
+
+		<div className='row'>
+			<div className='row text-left col'>
+				<FaClock className="col" style={{ color: "gray", fontSize: "1em" }} />
+				<p className="card-text col">
+					<small className="text-muted">10/10/10</small>
+				</p>
+
+			</div>
+			<div className='row text-left col'>
+				<FaEye className="col" style={{ color: "gray", fontSize: "1em" }} />
+				<p className="card-text col">
+					<small className="text-muted">10</small>
+				</p>
+
+			</div>
+
+		</div>
+
 		<img src="https://i0.wp.com/leonardmaltin.com/wp-content/uploads/2022/10/20tanya-tucker-review-videoSixteenByNineJumbo1600.jpg?resize=664%2C374&ssl=1" className="card-img-top" alt="..." width={"664px"} height={"374px"} />
 		<div className="card-body text-center">
 
