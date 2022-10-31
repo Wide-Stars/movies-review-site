7a868e408d0484a0cc6fdb2a54cc25aa0b077a38
Maruf Rien <maruf14222@gmail.com>
2022-11-01 01:13:42 +0600

    feat: add link to btn

--- src/components/blog/sideBar.js
+++ src/components/blog/sideBar.js
@@ -1,21 +1,50 @@
 import React from 'react'
 
 const sideBar = () => {
-	return <div className="card" style={{ width: "18rem" }}>
-		<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Cat_poster_1.jpg/260px-Cat_poster_1.jpg" className="card-img-top" alt="..." />
-		<div className="card-body">
-			<h5 className="card-title">Card title</h5>
-			<p className="card-text">
-				Some quick example text to build on the card title and make up the bulk of
-				the card's content.
-			</p>
-			<a href="#" className="btn btn-primary">
-				Go somewhere
-			</a>
+	return (<div>
+		<div className="card mt-5" style={{ width: "18rem" }}>
+			<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Cat_poster_1.jpg/260px-Cat_poster_1.jpg" className="card-img-top" alt="..." />
+			<div className="card-body">
+				<h5 className="card-title">Card title</h5>
+				<p className="card-text text-dark">
+					Some quick example text to build on the card title and make up the bulk of
+					the card's content.
+				</p>
+				<a href="#" className="btn btn-primary">
+					Go somewhere
+				</a>
+			</div>
 		</div>
+
+		<div className="card mt-5" style={{ width: "18rem" }}>
+			<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Cat_poster_1.jpg/260px-Cat_poster_1.jpg" className="card-img-top" alt="..." />
+			<div className="card-body">
+				<h5 className="card-title">Card title</h5>
+				<p className="card-text text-dark">
+					Some quick example text to build on the card title and make up the bulk of
+					the card's content.
+				</p>
+				<a href="#" className="btn btn-primary">
+					Go somewhere
+				</a>
+			</div>
+		</div>
+		<div className="card mt-5" style={{ width: "18rem" }}>
+			<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Cat_poster_1.jpg/260px-Cat_poster_1.jpg" className="card-img-top" alt="..." />
+			<div className="card-body">
+				<h5 className="card-title">Card title</h5>
+				<p className="card-text text-dark ">
+					Some quick example text to build on the card title and make up the bulk of
+					the card's content.
+				</p>
+				<a href="#" className="btn btn-primary">
+					Go somewhere
+				</a>
+			</div>
+		</div>
+
 	</div>
-
-
+	)
 }
 
 export default sideBar
