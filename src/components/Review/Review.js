3b7b359ff02c588e0fac0216a4f54499899f2016
Maruf Rien <maruf14222@gmail.com>
2022-12-01 00:51:24 +0600

    feat: add star icon in review

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -3,12 +3,33 @@ import classes from "./review.module.css";
 import { BsFillStarFill } from "react-icons/bs";
 
 const Review = (props) => {
+
+  const getStarts = (numberOfStars) => {
+    let stars = [];
+    for (let i = 0; i < numberOfStars; i++) {
+
+      stars.push(<BsFillStarFill />
+      )
+
+    }
+    return stars;
+  }
+
+
+  const htmlForStar = getStarts(props.rating)
+
+
+
+
   return (
     <div className={classes.review}>
+
+      {htmlForStar}
       <p>
-        <BsFillStarFill />
+
         {props.rating}\10
       </p>
+
       <h3>{props.title}</h3>
       <div className="name-date">
         <p>{props.name}</p>
