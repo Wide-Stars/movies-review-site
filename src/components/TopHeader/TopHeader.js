e3e22beeed3903df1a1772b1e7723a871506f53b
Maruf Rien <maruf14222@gmail.com>
2022-11-30 18:41:47 +0600

    feat: add search on top

--- src/components/TopHeader/TopHeader.js
+++ src/components/TopHeader/TopHeader.js
@@ -5,11 +5,22 @@ import Navigation from "./Navigation";
 import classes from "./topHeader.module.css";
 
 const TopHeader = () => {
+
+  const onSearchClicked = () => {
+    console.log("search clicked");
+  }
+
   return (
     <div className="mb-5">
       <header className={classes["top-header"]}>
         <Link className={classes["nav-brand"]} to="/"><RiMovie2Fill color="ff0000" />Film Craving</Link>
+
+        <input type="search" class="form-control rounded m-5" placeholder="Search" aria-label="Search" />
+        <button onClick={onSearchClicked} type="button" class="btn btn-outline-primary ">Search</button>
+
+
         <Navigation />
+
       </header>
       <hr />
     </div>
